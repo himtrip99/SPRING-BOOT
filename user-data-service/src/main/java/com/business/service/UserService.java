dd8c7627b0e5e192579d79e44e7384d02fc8a1f0
HimanshuT <himtrip99@gmail.com>
2019-04-03 03:22:09 +0100

    user data service initial commit

---
+++ user-data-service/src/main/java/com/business/service/UserService.java
@@ -0,0 +1,26 @@
+package com.business.service;
+
+
+import com.business.model.User;
+
+import java.util.List;
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers();
+	
+	void deleteAllUsers();
+	
+	boolean isUserExist(User user);
+	
+}
