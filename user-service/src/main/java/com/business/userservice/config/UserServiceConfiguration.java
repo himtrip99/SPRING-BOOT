8cae20c9f98bb9fae46c7c22afbbadb5313d3598
HimanshuT <himtrip99@gmail.com>
2019-04-12 19:33:13 +0100

    User Operations with Redis

---
+++ user-service/src/main/java/com/business/userservice/config/UserServiceConfiguration.java
@@ -0,0 +1,24 @@
+package com.business.userservice.config;
+
+import com.business.userservice.model.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class UserServiceConfiguration {
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+
+    }
+
+    @Bean
+    RedisTemplate<String, User> redisTemplate() {
+        RedisTemplate<String, User> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+    }
+}
